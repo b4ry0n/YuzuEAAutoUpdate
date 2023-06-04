4e71196b138d77f51ee31d0463cdddb4a766b2a0
b4ry0n <55383813+b4ry0n@users.noreply.github.com>
2023-06-04 11:43:54 +0200

    Check github url

--- UpdateYuzu.sh
+++ UpdateYuzu.sh
@@ -3,18 +3,15 @@
 APP_DIR="$HOME"/Applications
 YUZU_EA_LATEST="$APP_DIR"/yuzu-ea-latest.AppImage
 YUZU_EA_DIR="$APP_DIR"/YuzuEA
+YUZU_EA_GH="https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest"
 
-# Check if internet access exists
-if ping -q -c 1 -W 1 google.com >/dev/null; then
-
-    mkdir -p "$YUZU_EA_DIR"
-
-    asset=$(curl -s https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest | jq -r '.assets[] | select(.name|endswith(".AppImage"))')
-
+if resp="$(curl -sf $YUZU_EA_GH)"; then
+    asset=$(jq -nr --argjson resp "$resp" '$resp.assets[] | select(.name|endswith(".AppImage"))')
     filename=$(jq -nr --argjson asset "$asset" '$asset.name')
     download_url=$(jq -nr --argjson asset "$asset" '$asset.browser_download_url')
 
     if [[ ! -e "$YUZU_EA_DIR/$filename" ]]; then
+        mkdir -p "$YUZU_EA_DIR"
 
         # Download latest Yuzu EA
         curl -sL "$download_url" -o "$YUZU_EA_DIR/$filename"
@@ -25,7 +22,6 @@ if ping -q -c 1 -W 1 google.com >/dev/null; then
         # Link to latest
         ln -srf "$YUZU_EA_DIR/$filename" "$YUZU_EA_LATEST"
     fi
-
 fi
 
 # Launch Yuzu EA latest
